adc005b1d73d436575abbc83debdefb69e534b81
unknown <Dominic Freeman>
2015-07-25 07:49:44 +0300

    some crazy staff

--- js/script.js
+++ js/script.js
@@ -1,39 +1,12 @@
 (function(){
 
 	var pressButton = function(button, event, image){
-        var charCode = 0;
-        if(typeof button === 'string'){
-		    charCode = button.toUpperCase().charCodeAt(0);
-        } else {
-            charCode = button;
-        }
-		/*console.log(charCode);
-		var keyboardEvent = document.createEvent("KeyboardEvent");
-		
-        Object.defineProperty(keyboardEvent, 'keyCode', {
-            get : function() {
-                return this.keyCodeVal;
-            }
-        });
+		var charCode = 0;
+		if(typeof button === 'string')
+			charCode = button.toUpperCase().charCodeAt(0);
+		else
+			charCode = button;
 
-		Object.defineProperty(keyboardEvent, 'which', {
-			get : function() {
-				return this.keyCodeVal;
-			}
-		});
-
-        if (keyboardEvent.initKeyboardEvent) {
-            keyboardEvent.initKeyboardEvent(event, true, true, document.defaultView, false, false, false, false, charCode, charCode);
-        } else {
-            keyboardEvent.initKeyEvent(event, true, true, document.defaultView, false, false, false, false, charCode, 0);
-        }
-        keyboardEvent.keyCodeVal = charCode;
-
-        if (keyboardEvent.keyCode !== charCode) {
-            alert("keyCode mismatch " + keyboardEvent.keyCode + "(" + keyboardEvent.which + ")");
-        }
-
-		document.dispatchEvent(keyboardEvent);*/
 		$('body').trigger({type: event, which: charCode, keyCode: charCode});
 
 		addNotification(image);
@@ -50,23 +23,22 @@
 	};
 
 	var pikabu = function(action) {
-	  switch (action) {
-	    case "thumb_up":
-	      pressButton('w', 'keydown', 'thumbup');
-	      setTimeout(pressButton('d', 'keyup'), 2000);
-	      break;
-	    case "thumb_down":
-	      pressButton('s', 'keydown', 'thumbdown');
-	      setTimeout(pressButton('d', 'keyup'), 2000);
-	      break;
-	    case "v_sign":
-	      pressButton('r', 'keydown');
-	      setTimeout(pressButton('d', 'keyup'), 2000);
-	      break;
-	    default:
-	      return false;
-	  }
-	  return true;
+		switch (action) {
+			case "thumb_up":
+			pressButton('w', 'keydown', 'thumbup');
+			setTimeout(pressButton('d', 'keyup'), 1000);
+			break;
+			case "thumb_down":
+			pressButton('s', 'keydown', 'thumbdown');
+			setTimeout(pressButton('d', 'keyup'), 1000);
+			break;
+			case "v_sign":
+			pressButton('r', 'keydown');
+			break;
+			default:
+			return false;
+		}
+		return true;
 	};
 
 	/*
@@ -76,131 +48,134 @@
 
 		next: 		$('body').trigger({type: 'keydown', which: 39, keyCode: 39});
 		prev: 		$('body').trigger({type: 'keydown', which: 37, keyCode: 37});
-	*/
-	var imgur = function(action) {
-	  switch (action) {
-	    case "thumb_up":
-	      pressButton(107, 'keydown', 'thumbup');     
-	      setTimeout(pressButton(39, 'keydown'), 2000);
-	      break;
-	    case "thumb_down":
-	      pressButton(109, 'keydown', 'thumbdown');     
-	      setTimeout(pressButton(39, 'keydown'), 2000);
-	      break;
-	    case "tap": 
-        case "click":
-	      pressButton(96, 'keydown', 'thumbup');     
-	      setTimeout(pressButton(39, 'keydown'), 2000);
-	      break;
-	  } 
-	};
+		*/
+		var imgur = function(action) {
+			switch (action) {
+				case "thumb_up":
+				pressButton(107, 'keydown', 'thumbup');     
+				setTimeout(pressButton(39, 'keydown'), 1000);
+				break;
+				case "thumb_down":
+				pressButton(109, 'keydown', 'thumbdown');     
+				setTimeout(pressButton(39, 'keydown'), 1000);
+				break;
+				case "tap": 
+				case "click":
+				pressButton(96, 'keydown', 'thumbup');     
+				setTimeout(pressButton(39, 'keydown'), 1000);
+				break;
+				default:
+				return false;
+			}
+			return true; 
+		};
 
-	var startIntelSense = function(url) {
-	  var onConnect = function(data) {
-	      if (data.connected == false) {
-	          status('Alert: ' + JSON.stringify(data));
-	      }
-	  };
+		var startIntelSense = function(url) {
+			var onConnect = function(data) {
+				if (data.connected == false) {
+					status('Alert: ' + JSON.stringify(data));
+				}
+			};
 
-	  var timeout = false;
+			var timeout = false;
 
-		var clearTimeout = function() {
-			timeout = false;
-	  };
+			var clearTimeout = function() {
+				timeout = false;
+			};
 
-	  var onHandData = function(mid, module, data) {
-        if (data.hands === undefined || timeout) return;
-	      for (var g = 0; g < data.gestures.length; g++) {
+			var onHandData = function(mid, module, data) {
+				if (data.hands === undefined || timeout) return;
+				for (var g = 0; g < data.gestures.length; g++) {
 
-	          status(data.gestures[g].name);
+					status(data.gestures[g].name);
 
-	          if (url.indexOf('pikabu') != -1 && pikabu(data.gestures[g].name)){
-	              timeout = true;
-	              setTimeout(clearTimeout, 1000);
-	              return;
-	          }
-	          else if (url.indexOf('imgur') != -1 && imgur(data.gestures[g].name)){
-	              timeout = true;
-	              setTimeout(clearTimeout, 1000);
-	              return;
-	          }
-	          else if (pikabu(data.gestures[g].name)){
-	          	timeout = true;
-	          	setTimeout(clearTimeout, 1000);
-	          	return;
-	          }
-	      }
+					if (url.indexOf('pikabu') != -1 && pikabu(data.gestures[g].name)){
+						timeout = true;
+						setTimeout(clearTimeout, 1000);
+						return;
+					}
+					else if (url.indexOf('imgur') != -1 && imgur(data.gestures[g].name)){
+						timeout = true;
+						setTimeout(clearTimeout, 1000);
+						return;
+					}
+					else if (pikabu(data.gestures[g].name)){
+						timeout = true;
+						setTimeout(clearTimeout, 1000);
+						return;
+					}
+				}
 
-	  };
+			};
 
 
 
-	  var onStatus = function(data) {
-	      if (data.sts < 0) {
-	          status('Error ' + data.sts);
-	      }
-	  };
+			var onStatus = function(data) {
+				if (data.sts < 0) {
+					status('Error ' + data.sts);
+				}
+			};
 
-	  var status = function(msg) {
-	     console.log(msg);
-	  }
+			var status = function(msg) {
+				console.log(msg);
+			}
 
 	  // check platform compatibility
 	  RealSenseInfo(['hand'], function (info) {
-	      if (info.IsReady == true) {
-	          status('Started...');
-	      } else {
-	          status('ERROR');
-	      }
+	  	if (info.IsReady == true) {
+	  		status('Started...');
+	  	} else {
+	  		status('ERROR');
+	  	}
 	  });
 
 	  PXCMSenseManager_CreateInstance().then(function (result) {
-	      sense = result;
-	      return sense.EnableHand(onHandData);
+	  	sense = result;
+	  	return sense.EnableHand(onHandData);
 	  }).then(function (result) {
-	      handModule = result;
-	      status('Init started...');
-	      return sense.Init(onConnect, onStatus);
+	  	handModule = result;
+	  	status('Init started...');
+	  	return sense.Init(onConnect, onStatus);
 	  }).then(function (result) {
-	      status('CreateActiveConfiguration...');
-	      return handModule.CreateActiveConfiguration();
+	  	status('CreateActiveConfiguration...');
+	  	return handModule.CreateActiveConfiguration();
 	  }).then(function (result) {
-	      handConfiguration = result;
-	      status('DisableAllAlerts...');
-	      return handConfiguration.DisableAllAlerts();
+	  	handConfiguration = result;
+	  	status('DisableAllAlerts...');
+	  	return handConfiguration.DisableAllAlerts();
 	  }).then(function (result) {
-	      status('EnableAllGestures...');
-	      return handConfiguration.EnableAllGestures(false);
+	  	status('EnableAllGestures...');
+	  	return handConfiguration.EnableAllGestures(false);
 	  }).then(function (result) {
-	      status('ApplyChanges...');
-	      return handConfiguration.ApplyChanges();
+	  	status('ApplyChanges...');
+	  	return handConfiguration.ApplyChanges();
 	  }).then(function (result) {
-	      status('QueryCaptureManager...');
-	      return sense.QueryCaptureManager();
+	  	status('QueryCaptureManager...');
+	  	return sense.QueryCaptureManager();
 	  }).then(function (capture) {
-	      status('QueryImageSize...');
-	      return capture.QueryImageSize(pxcmConst.PXCMCapture.STREAM_TYPE_DEPTH);
+	  	status('QueryImageSize...');
+	  	return capture.QueryImageSize(pxcmConst.PXCMCapture.STREAM_TYPE_DEPTH);
 	  }).then(function (result) {
-	      imageSize = result.size;
-	      status('StreamFrames...');
-	      return sense.StreamFrames();
+	  	imageSize = result.size;
+	  	status('StreamFrames...');
+	  	return sense.StreamFrames();
 	  }).then(function (result) {
-	      status('Streaming ' + imageSize.width + 'x' + imageSize.height);
+	  	status('Streaming ' + imageSize.width + 'x' + imageSize.height);
 	  }).catch(function (error) {
-	      status('Init failed: ' + JSON.stringify(error));
+	  	status('Init failed: ' + JSON.stringify(error));
 	  });
 	};
 
 
 
 	var loadScript = function(url, callback){
-	    var head = document.getElementsByTagName('head')[0];
-	    var script = document.createElement('script');
-	    script.type = 'text/javascript';
-	    script.src = url;
-	    script.onreadystatechange = callback;
-	    script.onload = callback;
-	    head.appendChild(script);
+		var head = document.getElementsByTagName('head')[0];
+		var script = document.createElement('script');
+		script.type = 'text/javascript';
+		script.src = url;
+		script.onreadystatechange = callback;
+		script.onload = callback;
+		head.appendChild(script);
 	};
 
 	var doSomeAwesomeStuff = function(){
